23455699d378ed568f72302f4169baaf021d2994
Tara Thierry <tarathierry@gmail.com>
2022-09-03 18:13:25 -0400

    Add and change names for routes

---
+++ routes/journal.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const journalController = require('../controllers/journal') 
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
+
+router.get('/', ensureAuth, journalController.getJournal)
+
+router.post('/createTodo', journalController.createJournalEntry)
+
+//router.put('/markComplete', todosController.markComplete)
+
+//router.put('/markIncomplete', todosController.markIncomplete)
+
+router.delete('/deleteJournalEntry', journalController.deleteJournalEntry)
+
+module.exports = router
+
