e37e4c6513f254760cf874426d46e42c79ba7127
Tara Thierry <tarathierry@gmail.com>
2022-09-03 17:57:22 -0400

    Add and revise journal naming for model; first pass

---
+++ models/Journal.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const JournalSchema = new mongoose.Schema({
+  journalSubject: {
+    type: String,
+    required: true,
+  },
+  journalDate: {
+    type: String,
+    required: true,
+  },
+  journalEntry: {
+    type: String,
+    required: true,
+  },
+  /*completed: {
+    type: Boolean,
+    required: true,
+  },*/
+  userId: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Journal', JournalSchema)
